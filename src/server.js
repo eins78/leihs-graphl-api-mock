289dacf81c6c0ef2a1fbce6ddc6b730dc29a6f83
Max Albrecht <max.albrecht@zhdk.ch>
2019-02-08 17:05:02 +0100

    setup server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+import * as Schema from './schema'
+
+const PORT = 3700
+
+const apolloServer = new ApolloServer({
+  schema: Schema.schema
+})
+
+const server = express()
+apolloServer.applyMiddleware({ app: server })
+
+server.listen(PORT, () => {
+  console.log(
+    `🚀 GraphQL Server is now running on http://localhost:${PORT}/graphql`
+  )
+})
