098e35e4eae7c34f8f71f48c890e929d01b867f5
Max Albrecht <1@178.is>
2019-02-08 17:32:50 +0100

    server: redirect from root to playground

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import { ApolloServer, gql } from 'apollo-server-express'
-import * as Schema from './schema'
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import * as Schema from "./schema";
 
-const PORT = process.env.PORT || 3700
+const PORT = process.env.PORT || 3700;
 
 const apolloServer = new ApolloServer({
   schema: Schema.schema
-})
+});
 
-const server = express()
-apolloServer.applyMiddleware({ app: server })
+const server = express();
+apolloServer.applyMiddleware({ app: server });
+
+server.get("/", (req, res) => res.redirect("/graphql"));
 
 server.listen(PORT, () => {
   console.log(
     `🚀 GraphQL Server is now running on http://localhost:${PORT}/graphql`
-  )
-})
+  );
+});
